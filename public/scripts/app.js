f2c03593133329ebb191a9bfa860e352ea09cb99
Hibaq <hibaqwarsam79@gmail.com>
2023-02-15 21:58:49 +0000

    created functions to create and show shopping cart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -31,6 +31,7 @@ const loadDishes = function(callback) {
   console.log('work');
 };
 
+
 const buttonListeners = function(cart) {
   const buttons = document.querySelectorAll(".menu-item button");
 
@@ -58,6 +59,46 @@ const addToCart = (cart, id, product) => {
   console.log(cart);
 };
 
+const createShoppingCart = function() {
+  const $shoppingCartContainer = $('<div>').addClass('shopping-cart');
+  const $title = $('<div>').addClass('title').text('Shopping Cart');
+  const $item = $('<div>').addClass('item');
+  const $deleteBtnDiv = $('<div>').addClass('buttons');
+  const $deleteBtn = $('<span>').addClass('delete-btn');
+  const $deleteIcon = $('<i>').addClass('fa-solid fa-x');
+  const $imageDiv = $('<div>').addClass('image');
+  const $dishImage = $("<img/>", {
+    src:'pasta.jpeg'}).addClass('item-img');
+  const $descriptionDiv = $('<div>').addClass("description");
+  const $dishName = $('<span>').text("Spaghetti");
+  const $dishType = $('<span>').text('Main');
+  const $quantityContainer = $('<div>').addClass('quantity');
+  const $plusBtn = $('<button type="button">').addClass('plus-btn').attr('name','button');
+  const $plusIcon = $('<i>').addClass('fa-solid fa-plu');
+  const $input = $('<input type="text">').attr('name', 'name').text(1);
+  const $minusBtn = $('<button type="button">').addClass('minus-btn').attr('name','button');
+  const $minusIcon = $('<i>').addClass('fa-solid fa-minus');
+  const $dishCost = $('<div>').addClass('total-price').text('$549');
+  
+  $deleteBtn.append($deleteIcon);
+  $deleteBtnDiv.append($deleteBtn);
+  $imageDiv.append($dishImage);
+  $descriptionDiv.append($dishName,$dishType);
+  $plusBtn.append($plusIcon);
+  $minusBtn.append($minusIcon);
+  $quantityContainer.append($plusBtn,$input,$minusBtn,$dishCost);
+  $item.append($deleteBtnDiv, $imageDiv, $descriptionDiv, $quantityContainer);
+  $shoppingCartContainer.append($title,$item);
+
+  return $shoppingCartContainer;
+};
+
+const showShoppingCart =  function(){
+  const cart = createShoppingCart();
+  $('#item-container').append(cart);
+};
+
+
 $(document).ready(function() {
   let cart = {};
 
@@ -68,12 +109,15 @@ $(document).ready(function() {
 
   $( "#nav-icons" ).click(function() {
     alert( "Handler for .click() called." );
+    showShoppingCart();
     if($("#item-container").hasClass("blackout")){
       $("#item-container").removeClass("blackout").addClass("whiteout");
     } else {
       $("#item-container").removeClass("whiteout").addClass("blackout");
     }
   });
+
+  
 
   const promise1 = new Promise((resolve, reject) => {
     $.ajax({
